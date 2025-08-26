a13af42778eb7f6f8d135c91eb517cf78025efc9
Salaia <puma.hope@yandex.ru>
2025-08-26 13:51:33 +0300

    got data from url

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def run_pipeline():
+    # В качестве жертвы у меня librarianmon
+    api_url = "http://127.0.0.1:8182/users"  # Example API endpoint
+
+    # Make the GET request
+    response = requests.get(api_url)
+
+    # Check for successful response (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response into a Python dictionary or list
+        data = response.json()
+        print("Data retrieved successfully:")
+        print(data)
+    else:
+        print(f"Error: Unable to retrieve data. Status code: {response.status_code}")
+        print(response.text)  # Print the error message if available
+
+
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run_pipeline()
